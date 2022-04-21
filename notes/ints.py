3978d1fc21ddaa26932f7befad3e4c31b36225ff
bartserafin <86855505+bartserafin@users.noreply.github.com>
2022-04-21 14:49:20 +0200

    Add files via upload

---
+++ notes/ints.py
@@ -0,0 +1,14 @@
+### INT ###
+
+# + add
+# - substract
+# * multiply
+# / divide
+# % modulo
+# ** power
+
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+print(x + y)
+
+print(int(input("x= ")) + int(input("y= ")))
