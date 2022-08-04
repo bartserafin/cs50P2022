03715f255a581801f75112b82ab255d123ab7f3a
bartserafin <86855505+bartserafin@users.noreply.github.com>
2022-08-04 14:07:52 +0200

    Add files via upload

---
+++ week5/notes/test_calculator.py
@@ -0,0 +1,23 @@
+from calculator import square
+
+
+# def test_square():
+#     assert square(2) == 4
+#     assert square(3) == 9
+#     assert square(-2) == 4
+#     assert square(-3) == 9
+#     assert square(0) == 0
+
+
+def test_positive():
+    assert square(2) == 4
+    assert square(3) == 9
+
+
+def test_negative():
+    assert square(-2) == 4
+    assert square(-3) == 9
+
+
+def test_zero():
+    assert square(0) == 0
---
+++ week5/notes/test_hello.py
@@ -0,0 +1,10 @@
+from hello import hello
+
+
+def test_defualt():
+    assert hello() == "hello, world"
+
+
+def test_argument():
+    for name in ["Hermione", "Harry", "Ron"]:
+        assert hello(name) == f"hello, {name}"
